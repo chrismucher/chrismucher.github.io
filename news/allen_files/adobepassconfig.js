084cca40a615e12a760482231c14387a726ae9e3
Stephen Mucher <smucher@codonics.com>
2021-09-22 17:42:07 -0400

    add news template

---
+++ news/allen_files/adobepassconfig.js
@@ -0,0 +1,25 @@
+AdobePassConfig({
+	"env":		{
+			"pc":true,"ios":true,"android":true		},	"mvpds":[		{
+			"id":"ATT","name":"AT&T U-verse","status":true		},		{
+			"id":"Cox","name":"Cox","status":true		},		{
+			"id":"DTV","name":"DIRECTV","status":true		},		{
+			"id":"ATTOTT","name":"AT&T TV NOW","status":true		},		{
+			"id":"Dish","name":"DISH","status":true		},		{
+			"id":"Verizon","name":"Verizon Fios","status":true		},		{
+			"id":"Cablevision","name":"Optimum","status":true		},		{
+			"id":"Spectrum","name":"Spectrum","status":true		},		{
+			"id":"Comcast_SSO","name":"Xfinity","status":true,"links":{"pc":"https://xfinity.com/stream","ios":"https://itunes.apple.com/us/app/xfinity-tv/id731629156?ls=1&mt=8","android":"https://play.google.com/store/apps/details?id=com.xfinity.cloudtvr"}		},		{
+			"id":"www_websso_mybrctv_com","name":"Blue Ridge Communications","status":true		},		{
+			"id":"frontier_auth-gateway_net","name":"Frontier Communications","status":true		},		{
+			"id":"buc020","name":"Buckeye Broadband","status":true		},		{
+			"id":"AlticeOne","name":"Altice One","status":true		},		{
+			"id":"gta_auth-gateway_net","name":"GTA","status":true		},		{
+			"id":"lib040","name":"Liberty Cablevision of PR","status":true		},		{
+			"id":"RCN","name":"RCN","status":true		},		{
+			"id":"mar050","name":"Summit Broadband","status":true		},		{
+			"id":"YouTubeTV","name":"YouTube TV","status":true		},		{
+			"id":"slingtv","name":"Sling TV","status":true		},		{
+			"id":"layer3tv_auth-gateway_net","name":"T-Mobile","status":true		},		{
+			"id":"ziplyfiber_auth-gateway_net","name":"Ziply Fiber","status":true		}		]
+				,"requestorId":"NBADE"		,"softwareStatement":"eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJjZjE2NGE0ZC01MzBkLTRhMTItYTg0Mi03NjVjYTMwMzFhOGQiLCJuYmYiOjE1ODUzNDQyMjMsImlzcyI6ImF1dGguYWRvYmUuY29tIiwiaWF0IjoxNTg1MzQ0MjIzfQ.J_4hGzhPek1sDoi8hbLZETP7BdD5pCmZRo22N40GLgHRRDGnHNNmnQKrcNrhgKUz5oTbCvn0sz6Z-hBB2sNJWwRIPsVAit9OBIMGrcG0ToAq-dmdtwbSXfJFKh2PgyzjkmnmEVK61oIA9YnWnXUkmKudSrEk8D6FuBuMOl3_gHDl99VErNDRyMNGnfs0KktbWwYqgaoPFeGaMio-D-4vRdyePjtI6T_XJcCIrdcc6YjuspSDmz7dbCvzBt_p1EknWe-n2TxaeaP6J6tAG_3LB5Bus3xVRLxov0Tujt8by8J1qJBuOqLHQUFXYssLl895HT-zWusxzOduaK-TNNKeVw"		})
